5c69cdea43a31b2bf2b58d675e7aa9dc1c514423
Tirth S Pandya <166584587+Flintiee@users.noreply.github.com>
2024-06-11 12:01:44 +0530

    commit

---
+++ Chapter1/DataTypes.py
@@ -0,0 +1,16 @@
+# variables:
+# any entity whose value can be changed.
+# a = 32 this is called integer datatype
+# b = 3.246 this is called float datatype
+# c = "kedaar" this is called string datatype
+# d = True this is called boolean datatype 
+# boolean has only 2 possible answers True/False.
+# e = none this is called none datatype.
+# it represents nothing.
+# -------------------------------------------------------
+# print(type(a))
+# printes the value of a.
+
+
+
+
