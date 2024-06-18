1e78f18e8c6c07b7644699d2f67850d8bb886cba
Tirth S Pandya <166584587+Flintiee@users.noreply.github.com>
2024-06-18 18:02:41 +0530

    commit

--- Chapter1/slice.py
+++ Chapter1/slice.py
@@ -1,4 +1,9 @@
-string = "kedaar"
-# result = string.count("A")
-result = string.find("a")
+string = "kedaar is a good boi"
+# result = string.count("A") A kitne hai
+# result = string.find("a") same -_-
+# result = string.capatalize() pehele letter ko capatalize kr dega
+# result = string.endswith("ar") return krega agar true hai
+# result = string.endswith("or") ya vica versa
+# result = string.find("good") index return krega true false nhi
+# result = string.replace("boi", "gurl") replaces the word
 print(result)
