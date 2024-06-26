8855c6c5393b57e1164ee8e5a635bc4107190974
Tirth S Pandya <teamfznltd@gmail.com>
2024-06-26 10:29:12 +0000

    commit

--- Chapter1/dict.py
+++ Chapter1/dict.py
@@ -7,5 +7,9 @@ fruits = {
     "Ice": "Baraf",
     "Num": [12,34,54544,10]
 }
-new = fruits["Key"]
+# new = fruits["Key"]
+# new = fruits.items()
+# new = fruits.keys()
+new = fruits.update({"Tirth": "Good boi"})
 print(new)
+
