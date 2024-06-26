108ca6db109873950755b0545bb64be56dba5668
Tirth S Pandya <teamfznltd@gmail.com>
2024-06-26 10:50:16 +0000

    commit

--- Chapter1/nothing.c
+++ Chapter1/nothing.c
@@ -1,7 +1,24 @@
 #include<stdio.h>
+struct students{
+    int id;
+    float percentile;
+    char name[50];
+};
 int main(){
     int i;
-    printf("hello git\n");
+    struct students e1,e2,e3;
+    e1.id = 43;
+    e2.id = 44;
+    e1.percentile = 56.67;
+    e2.percentile = 89.99;
+    strcpy(e1.name, "Tirth");
+    // e2.name = "Harry";
+    printf("the id of the first student = %d\n", e1.id);
+    // printf("the name of the student = %s\n");
+    printf("the name of the student = %s\n", e1.name);
+    
+
+
 
     return 0;
 }
