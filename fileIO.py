fe8a963fa7ad10ad389bf74a68c4b9351444c52e
Tirth S Pandya <teamfznltd@gmail.com>
2024-06-27 11:05:12 +0000

    commit

--- fileIO.py
+++ fileIO.py
@@ -1,1 +1,8 @@
-f = open("example.txt", "r")
+# f = open("read.txt", mode="w")
+# text = f.read()
+# print(text)
+# f.write("Kedaar is good boiii\n")
+# f.write("wow i wrote hello world in a file!!\n")
+# f.write("Hello, world!\n")
+# with open("example.txt", "w") as f:
+#     f.read()
