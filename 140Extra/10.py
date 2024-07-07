6fdb17da699a4e9ba350084e0ada545727aaedc9
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-07 08:02:17 +0000

    commit

---
+++ 140Extra/10.py
@@ -0,0 +1,7 @@
+# Write a Python program to swap two variables without temp variable
+a = 12
+b = 43
+a,b = b,a
+print("after swapping\n")
+print("a=",a)
+print("b=",b)
---
+++ 140Extra/11.py
@@ -0,0 +1,10 @@
+# Write a Python Program to Check if a Number is Positive, Negative or Zero.
+x = int(input("enter value\n"))
+if x>0:
+    print("the entity is positive\n")
+
+elif x == 0:
+    print("the entity is equal to 0\n")
+
+else:
+    print("the entity is negative\n")
