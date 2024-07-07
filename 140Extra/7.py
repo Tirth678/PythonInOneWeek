3a7ee4e5c48956727b196d75a4c69c631fdc8432
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-07 07:59:02 +0000

    commit

---
+++ 140Extra/7.py
@@ -0,0 +1,4 @@
+# Write a Python program to convert Celsius to Fahrenheit.
+cel = int(input("enter temprature\n"))
+TempConvo = cel*(1.8)+32
+print("temprature in fahrenehit =", TempConvo)
---
+++ 140Extra/8.py
@@ -0,0 +1,6 @@
+# Write a Python program to display calendar
+import calender 
+year = int(input("enter year\n"))
+month = int(input("enter month\n"))
+cal = calender.month(month, year)
+print(cal)
