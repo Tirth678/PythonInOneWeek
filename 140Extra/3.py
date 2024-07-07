24617c83d888f0ff7938cba7fba531063a922e39
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-07 07:40:12 +0000

    commit

---
+++ 140Extra/3.py
@@ -0,0 +1,5 @@
+# Write a Python program to find the area of a triangle.
+breath = int(input("enter breath of traingle\n"))
+length = int(input("enter length of triangle\n"))
+area = 1/2*(length*breath)
+print("the area of traingle =", area)
