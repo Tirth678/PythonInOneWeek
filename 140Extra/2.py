8d18dd244296904cf7ac96ef95a90b08dae61dc3
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-07 07:37:41 +0000

    commit

---
+++ 140Extra/2.py
@@ -0,0 +1,11 @@
+# Write a Python program to do arithmetical operations addition and divisio
+a = int(input("enter the first value\n"))
+b = int(input("enter the second value\n"))
+addition = a+b
+divison = a/b
+subtraction = a-b
+multiplication = a*b
+print("the sum of a and b =", addition)
+print("the subtraction of a and b =", subtraction)
+print("the multiplication of a and b =", multiplication)
+print("the divison of a and b =", divison)
