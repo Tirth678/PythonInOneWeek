79d7b1e0b8c64ff0ff0c4dd5960c178196c31aa9
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-09 14:21:04 +0530

    commit

---
+++ 140Extra/21.py
@@ -0,0 +1,9 @@
+# x = int(input("enter the first value\n"))
+# y = int(input("enter the second value\n"))
+# z = int(input("enter the third value\n"))
+# sum = x+y+z
+# print("the sum of the 3 numbers =", sum)
+sum = 0
+for ele in range(11):
+    print(sum)
+    sum += sum+1
