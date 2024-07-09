d7555ab3a01b54aa3d997349d9b96a8d3fe8ad24
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-09 12:16:50 +0000

    commit

---
+++ TheUltimatePythonHandbook/chap8-6.py
@@ -0,0 +1,8 @@
+def inToCm(x):
+    if x == 0:
+        return 0
+    else:
+        return x*2.54
+    
+num = 23
+print(inToCm(num))
---
+++ TheUltimatePythonHandbook/chap8-8.py
@@ -0,0 +1,15 @@
+def tableof(x):
+    if x == 0:
+        return 0
+    else:
+        for i in range(11):
+            i = 0
+            i=i+1
+            return x*(i+0)
+        
+
+
+
+        
+num = int(input("enter a value\n"))
+print(tableof(num))
